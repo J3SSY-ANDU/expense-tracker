e4e74f5c2498966bdf4cba892a0f31e8c54f216b
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-01-16 22:49:59 -0400

    updated the verification email frontend

--- frontend/src/components/VerifyEmail.tsx
+++ frontend/src/components/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { VerifyEmail as VerifyEmailApi, ResendVerificationEmail } from "../api";
 
@@ -28,9 +28,32 @@ export function VerifyEmail() {
   }
 
   return (
-    <Box>
-      <Typography variant="h4">Verify your email</Typography>
-      <Typography>
+    <Paper
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "0.8rem",
+        width: "20%",
+        minWidth: "500px",
+        padding: "1.5rem 1rem",
+        background: "#fff",
+        border: "1px solid #d3d3d3",
+        borderRadius: "4px",
+        margin: "auto",
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: "fit-content",
+      }}
+    >
+      <Typography variant="h4" sx={{ fontWeight: "600", fontSize: "2rem" }}>
+        Verify your email
+      </Typography>
+      <Typography variant="body1" sx={{ fontSize: "1rem", fontWeight: "400" }}>
         We have sent you an email with a verification link. Please click on the
         link to verify your email.
       </Typography>
@@ -41,6 +64,6 @@ export function VerifyEmail() {
       >
         Resend email
       </Button>
-    </Box>
+    </Paper>
   );
 }
