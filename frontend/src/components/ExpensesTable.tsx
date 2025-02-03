101bf1259fc3a80624fffed0bdaf5b685c090550
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-02-03 13:24:55 -0400

    Implemented inline editing and saving for category and expense names

--- frontend/src/components/ExpensesTable.tsx
+++ frontend/src/components/ExpensesTable.tsx
@@ -549,6 +549,7 @@ export function ExpensesTable({
         handleChangeDate={handleChangeDate}
         handleChangeNotes={handleChangeNotes}
         handleDeleteExpense={handleDeleteExpense}
+        handleChangeExpenseName={handleChangeExpenseName}
       />
     </Box>
   );
