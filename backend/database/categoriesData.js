191361ea1ada387ac9ad7cded99bfc4f50a18fbf
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-06-23 20:17:50 -0400

    remove categoriesData.js from .gitignore and add categoriesData.js file with initial category data

---
+++ backend/database/categoriesData.js
@@ -0,0 +1,44 @@
+const categoriesData = [
+  {
+    name: "Bills & Utilities",
+    total_expenses: 0,
+    description: "Electricity, water, gas, internet, phone, etc.",
+  },
+  {
+    name: "Education",
+    total_expenses: 0,
+    description: "Tuition, books, supplies, etc.",
+  },
+  {
+    name: "Entertainment",
+    total_expenses: 0,
+    description: "Movies, concerts, games, etc.",
+  },
+  {
+    name: "Food & Groceries",
+    total_expenses: 0,
+    description: "Eating out, groceries, snacks, etc.",
+  },
+  {
+    name: "Health & Fitness",
+    total_expenses: 0,
+    description: "Gym, doctor, dentist, pharmacy, etc.",
+  },
+  {
+    name: "Shopping",
+    total_expenses: 0,
+    description: "Clothes, shoes, accessories, etc.",
+  },
+  {
+    name: "Transportation",
+    total_expenses: 0,
+    description: "Gas, public transportation, parking, etc.",
+  },
+  {
+    name: "Travel",
+    total_expenses: 0,
+    description: "Flights, hotels, car rentals, etc.",
+  }
+];
+
+module.exports = { categoriesData };
