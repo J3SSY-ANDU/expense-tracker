22c3bfe338cc1f93355d74c732525968d48496e9
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-07-28 15:22:09 -0400

    feat: refactor Expenses and Layout components to use Outlet context for state management

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -2,34 +2,51 @@ import { useEffect, useState } from 'react';
 import { Account } from './components';
 import NavTabs from './NavTabs';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { User } from './types';
+import { Category, Expense, User, History as MonthlyHistory } from './types';
 import apiService from './api/apiService';
 
 export default function Layout() {
     const [user, setUser] = useState<User | null>(null);
+    const [categories, setCategories] = useState<Category[] | null>(null);
+    const [expenses, setExpenses] = useState<Expense[] | null>(null); // State for fetched data
+    const [history, setHistory] = useState<MonthlyHistory[] | null>(null);
+    const [errors, setErrors] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchUser = async () => {
+        const fetchData = async () => {
             setLoading(true);
+            setErrors(null);
             try {
-                const userData: User | { error: string } = await apiService.getUserData();
-                if ((!userData || "error" in userData)) {
-                    setUser(null);
-                    navigate("/login");
+                // Fetch all data
+                const [userData, categoriesData, expensesData, historyData] = await Promise.all([
+                    apiService.getUserData(),
+                    apiService.getCategoriesData(),
+                    apiService.getExpensesData(),
+                    apiService.getHistoryData()
+                ]);
+
+                if ((!userData || "error" in userData) || (!categoriesData || "error" in categoriesData) || (!expensesData || "error" in expensesData) || (!historyData || "error" in historyData)) {
+                    setErrors("Failed to fetch data.");
                     return;
-                } 
+                }
+                
                 setUser(userData);
+                setCategories(categoriesData);
+                setExpenses(expensesData);
+                setHistory(historyData);
+
                 setLoading(false);
             } catch (error) {
-                console.error("Error fetching user data:", error);
+                console.error("Error fetching data:", error);
+                setErrors("Something went wrong while fetching data. Please try again later.");
                 setLoading(false);
                 return;
             }
         };
-        fetchUser();
-    }, [navigate]);
+        fetchData();
+    }, []);
 
     return (
         <div>
@@ -37,7 +54,7 @@ export default function Layout() {
                 <Account user={user} setUser={setUser} />
                 <NavTabs />
             </div>
-            <Outlet />
+            <Outlet context={{ categories, setCategories, expenses, setExpenses, history, setHistory }} />
         </div>
     );
 }
