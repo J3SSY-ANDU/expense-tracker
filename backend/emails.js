96f3911e26ae8f078ea964239f364b39029879ac
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-08-20 23:32:09 -0400

    feat: add JSDoc comments to expenses and users modules for improved documentation and clarity

--- backend/emails.js
+++ backend/emails.js
@@ -1,85 +1,118 @@
-const nodemailer = require("nodemailer");
-const dotenv = require("dotenv").config({
-    path:
-        process.env.NODE_ENV === "production"
-            ? ".env.production"
-            : ".env.development",
-});
-const { getForgotPasswordToken } = require("./database/forgotPassword");
-const { getUserById } = require("./database/users");
-const { createEmailVerificationToken } = require("./database/emailVerification");
+const nodemailer = require('nodemailer')
+const dotenv = require('dotenv').config({
+  path:
+    process.env.NODE_ENV === 'production'
+      ? '.env.production'
+      : '.env.development'
+})
+const { getForgotPasswordToken } = require('./database/forgotPassword')
+const { getUserById } = require('./database/users')
+const { createEmailVerificationToken } = require('./database/emailVerification')
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-    },
-});
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD
+  }
+})
 
-const sendEmailVerification = async (email) => {
-    try {
-        const token = await createEmailVerificationToken(email);
+/**
+ * Sends an email verification link to the specified email address.
+ *
+ * Generates a unique verification token, constructs a verification URL,
+ * and sends an email containing the verification link to the user.
+ * The verification link expires in half an hour.
+ *
+ * @async
+ * @function
+ * @param {string} email - The recipient's email address.
+ * @throws Will throw an error if sending the email fails.
+ */
+const sendEmailVerification = async email => {
+  try {
+    const token = await createEmailVerificationToken(email)
 
-        const verificationUrl = `${process.env.CLIENT_URL}/verify-email/${token}`;
+    const verificationUrl = `${process.env.CLIENT_URL}/verify-email/${token}`
 
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: "Account Verification",
-            html: `
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Account Verification',
+      html: `
                 <h1>Verify Your Email</h1>
                 <p>Thank you for signing up! Please verify your email by clicking the link below:</p>
                 <a href="${verificationUrl}" target="_blank">Verify Email</a>
                 <p>This link will expire in half an hour.</p>
-            `,
-        };
-
-        await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully!");
-    } catch (err) {
-        console.error(`Error sending email: ${err}`);
-        throw err;
+            `
     }
+
+    await transporter.sendMail(mailOptions)
+    console.log('Email sent successfully!')
+  } catch (err) {
+    console.error(`Error sending email: ${err}`)
+    throw err
+  }
 }
 
-const forgotPasswordEmail = async (email) => {
-    try {
-        const token = await getForgotPasswordToken(email);
-        if (!token) {
-            console.log("Token not found.");
-            throw new Error("TOKEN_NOT_FOUND");
-        }
+/**
+ * Sends a password reset email to the specified email address.
+ *
+ * This function generates a password reset token, constructs a reset URL,
+ * and sends an email with the reset link to the user. The reset link expires in 30 minutes.
+ *
+ * @async
+ * @param {string} email - The email address of the user requesting a password reset.
+ * @throws {Error} Throws an error if the token is not found or if sending the email fails.
+ * @returns {Promise<void>} Resolves when the email is sent successfully.
+ */
+const forgotPasswordEmail = async email => {
+  try {
+    const token = await getForgotPasswordToken(email)
+    if (!token) {
+      console.log('Token not found.')
+      throw new Error('TOKEN_NOT_FOUND')
+    }
 
-        const resetUrl = `${process.env.CLIENT_URL}/reset-forgot-password?token=${token}`;
+    const resetUrl = `${process.env.CLIENT_URL}/reset-forgot-password?token=${token}`
 
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: "Reset Password",
-            html: `
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Reset Password',
+      html: `
                 <h1>Reset Your Password</h1>
                 <p>You requested a password reset. Click the link below to reset your password:</p>
                 <a href="${resetUrl}" target="_blank">Reset Password</a>
                 <p>This link will expire in half an hour.</p>
-            `,
-        };
-
-        await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully!");
-    } catch (err) {
-        console.error(`Error sending email: ${err}`);
-        throw err;
+            `
     }
+
+    await transporter.sendMail(mailOptions)
+    console.log('Email sent successfully!')
+  } catch (err) {
+    console.error(`Error sending email: ${err}`)
+    throw err
+  }
 }
 
+/**
+ * Sends an account deletion confirmation email to the specified user.
+ *
+ * @async
+ * @function
+ * @param {string} name - The name of the user whose account was deleted.
+ * @param {string} email - The email address of the user to send the confirmation to.
+ * @returns {Promise<void>} Resolves when the email has been sent successfully.
+ * @throws Will throw an error if sending the email fails.
+ */
 const deleteAccountEmail = async (name, email) => {
-    try {
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: "Account Deletion Confirmation",
-            html: `
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Account Deletion Confirmation',
+      html: `
                 <p>Hey <strong>${name}</strong>,</p>
                 <p>
                     Just a quick note to let you know that your <strong>Expense Tracker</strong> account has been successfully deleted.
@@ -91,15 +124,19 @@ const deleteAccountEmail = async (name, email) => {
                     <li>✅ If you did this by mistake or change your mind, let us know—we’re happy to help!</li>
                 </ul>
                 <p>Thanks for trying <strong>Expense Tracker</strong>! We appreciate you and hope to see you again in the future.</p>
-            `,
-        };
-
-        await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully!");
-    } catch (err) {
-        console.error(`Error sending email: ${err}`);
-        throw err;
+            `
     }
+
+    await transporter.sendMail(mailOptions)
+    console.log('Email sent successfully!')
+  } catch (err) {
+    console.error(`Error sending email: ${err}`)
+    throw err
+  }
 }
 
-module.exports = { sendEmailVerification, forgotPasswordEmail, deleteAccountEmail };
+module.exports = {
+  sendEmailVerification,
+  forgotPasswordEmail,
+  deleteAccountEmail
+}
